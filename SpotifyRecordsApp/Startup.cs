855a68de350e41ddb0d46e0d521b7a2a3e67f946
Mauricio Guzmán Salazar <mauricio.guzman.salazar@gmail.com>
2016-09-18 18:41:39 -0500

    Initial project setup, added database, created database model

---
+++ SpotifyRecordsApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SpotifyRecordsApp.Startup))]
+namespace SpotifyRecordsApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
